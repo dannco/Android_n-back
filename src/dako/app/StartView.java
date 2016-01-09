e10ad9ea3808a578a97447e7583b691fde696e2d
Danne <danne.collin@gmail.com>
2016-01-09 13:45:42 +0100

    first commit

---
+++ src/dako/app/StartView.java
@@ -0,0 +1,11 @@
+package dako.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartView extends Activity {
+	protected void onCreate(Bundle bndl) {
+		super.onCreate(bndl);		
+		setContentView(null);
+	}
+}
