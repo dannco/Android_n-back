b2f3b24b642e772613ca8830eba8cbba4e2b8180
Danne <danne.collin@gmail.com>
2016-01-10 14:11:20 +0100

    added setup and main activities

---
+++ src/dako/app/Setup.java
@@ -0,0 +1,12 @@
+package dako.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Setup extends Activity {
+	protected void onCreate(Bundle bndl) {
+		super.onCreate(bndl);
+		setContentView(null);		
+	}
+	
+}
